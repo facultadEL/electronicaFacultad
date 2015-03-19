d79b48837da2242fea2e2c59bcd388464583ab5a
Lucas <lpm19.2009@gmail.com>
2015-03-18 22:50:39 -0300

    modificación del login

--- login.php
+++ login.php
@@ -11,7 +11,7 @@
 		<h2><span class="fontawesome-lock"></span>Login</h2>
 		<form action="" method="POST">
 			<fieldset>
-				<p><label for="email">E-mail</label></p>
+				<p><label for="email">Usuario</label></p>
 				<p><input type="email" id="email" value="" placeholder="E-mail"/></p> <!-- JS because of IE support; better: placeholder="mail@address.com" -->
 
 				<p><label for="password">Contrase&ntilde;a</label></p>
@@ -20,6 +20,8 @@
 				<p><input type="submit" value="Acceder"></p>
 				<p><input type="button" value="Olvid&eacute; mi contrase&ntilde;a"></p>
 			</fieldset>
+				<p><input type="button" id="btn_sincta" value="No tengo cuenta"></p>
+
 		</form>
 	</div> <!-- end login -->
 </body>	
