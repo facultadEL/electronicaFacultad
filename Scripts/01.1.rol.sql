d863d9ebfdf22f1bf554627581fca538adec3212
Lucas <lpm19.2009@gmail.com>
2015-03-24 21:08:09 -0300

    script faltante

---
+++ Scripts/01.1.rol.sql
@@ -0,0 +1,8 @@
+CREATE TABLE rol
+(
+	id serial NOT NULL primary key,
+	nombre character varying
+);
+INSERT INTO rol(nombre)VALUES('Alumno');
+INSERT INTO rol(nombre)VALUES('Administrador');
+INSERT INTO rol(nombre)VALUES('Profesor');
