fc3825adb2059c7f113c2abfe3a7d1c8b1a6815e
Lucas <lpm19.2009@gmail.com>
2015-11-10 18:04:09 -0300

    probando git ignore

--- conexion.php
+++
@@ -1,5 +0,0 @@
-<?php
-$conn = pg_connect("host=localhost port=5432 user=postgres password=postgres dbname=ppselectronica") or die("Error de conexion.".pg_last_error());//conexion local
-
-//$conn = pg_connect("host=190.114.198.126 port=5432 user=extension password=newgenius dbname=ppselectronica") or die("Error de conexion.".pg_last_error()); //conexion facu
-?>
